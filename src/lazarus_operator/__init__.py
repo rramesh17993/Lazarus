b8433d4d1305f7d88819c146e91d5c43d3b093fa
rramesh17993 <rramesh17993@gmail.com>
2026-01-01 08:08:13 +0530

    Update __init__.py

--- src/lazarus_operator/__init__.py
+++ src/lazarus_operator/__init__.py
@@ -1,5 +1,5 @@
 """Lazarus Operator - Automated Backup Recovery Validation for Kubernetes."""
 
 __version__ = "0.1.0"
-__author__ = "Your Name"
+__author__ = "Rajesh Ramesh"
 __license__ = "Apache-2.0"
